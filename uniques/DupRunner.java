e965e949f42fef07c9b2e9afc9c98e54b12711a1
mr-poston <john.poston@risd.org>
2020-07-29 21:42:45 +0000

    Add Uniques

---
+++ uniques/DupRunner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+import static java.lang.System.*;
+
+public class DupRunner
+{
+    public static void main(String[] args)
+    {
+        // Add tests
+    }
+}
---
+++ uniques/UniquesDupes.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class UniquesDupes
+{
+    public static Set<String> getUniques(String input)
+    {
+        Set<String> uniques = new TreeSet<>();
+
+        // Add code
+
+        return uniques;
+    }
+
+    public static Set<String> getDupes(String input)
+    {
+        // Add code
+
+        return null;
+    }
+}
