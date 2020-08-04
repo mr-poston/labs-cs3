3e6f0e52693c530258e845c5002ad111210e669a
mr-poston <john.poston@risd.org>
2020-08-04 21:47:23 +0000

    Added Spanish to English

---
+++ spanish/SpanRunner.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SpanRunner {
+    public static void main(String[] args) {
+        try {
+
+        } catch (IOException e) {
+            
+        }
+    }
+}
---
+++ spanish/SpanishToEnglish.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class SpanishToEnglish {
+    private Map<String, String> pairs;
+
+    public SpanishToEnglish() {
+
+    }
+
+    public void putEntry(String entry) {
+
+    }
+
+    public String translate(String sent) {
+        Scanner scan = new Scanner(sent);
+        String output = "";
+
+
+        return output;
+    }
+
+    public String toString() {
+        return pairs.toString().replaceAll("\\,", "\n");
+    }
+}
