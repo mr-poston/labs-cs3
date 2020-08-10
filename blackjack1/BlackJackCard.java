d19f9ae80edee781f95b51acabbfd3dd4c28273f
mr-poston <john.poston@risd.org>
2020-08-10 00:47:09 +0000

    Several Updates

---
+++ blackjack1/BlackJackCard.java
@@ -0,0 +1,13 @@
+public class BlackJackCard extends Card {
+
+    // Constructors
+
+
+    @Override
+    public int getValue() {
+        // Enables you to build the value for the game into the card.
+        // This makes writing the whol program a little easier.
+
+        return 0;
+    }
+}
