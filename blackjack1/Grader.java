7a6637f12e0977e2e73ab2ec86be1c53b5a0f8c8
mr-poston <john.poston@risd.org>
2020-08-10 14:58:19 +0000

    Update Grader

--- blackjack1/Grader.java
+++ blackjack1/Grader.java
@@ -8,9 +8,20 @@ public class Grader {
         if (args.length == 0) {
             Card card = new BlackJackCard();
             out.println(card);
-        } else {
+        } else if (args.langth == 2) {
             Card card = new BlackJackCard(Integer.parseInt(args[0]), args[1]);
             out.println(card);
+        } else if (args.length == 1 && args[0].equals("equals")) {
+            Card one = new BlackJackCard();
+            Card two = new BlackJackCard(1, "DIAMONDS");
+            Card three = new BlackJackCard(4, "CLUBS");
+            Card four  = new BlackJackCard(12, "SPADES");
+            Card five = new BlackJackCard(12, "HEARTS");
+            Card six = new BlackJackCard(9, "SPADES");
+            out.println(one.equals(two));
+            out.println(one.equals(one));
+            out.println(four.equals(five));
+            out.println(three.equals(four));
         }
     }
 }
