d2884fb55d93301805caf8832913abae03ad3f28
mr-poston <john.poston@risd.org>
2020-09-03 13:55:07 +0000

    Test blackjack4

--- blackjack4/Grader.java
+++ blackjack4/Grader.java
@@ -5,6 +5,19 @@ import static java.lang.System.*;
 
 public class Grader {
     public static void main(String[] args) {
-        
+        if (args.length != 1) {
+            return;
+        }
+        Dealer dealer = new Dealer();
+        dealer.resetHand();
+        dealer.addCardToHand(new BlackJackCard(1, "HEARTS"));
+        if (args[0].equals("yes")) {
+            dealer.addCardToHand(new BlackJackCard(5, "CLUBS"));
+        } else if (args[0].equals("no")) {
+            dealer.addCardToHand(new BlackJackCard(6, "SPADES"));
+        } else {
+            return;
+        }
+        out.println(dealer.hit();
     }
 }
