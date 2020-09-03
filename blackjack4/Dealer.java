e2013fea7ba63d2a9ca4633a521268263d063bc8
mr-poston <john.poston@risd.org>
2020-09-03 02:08:51 +0000

    Add blackjack4

---
+++ blackjack4/Dealer.java
@@ -0,0 +1,40 @@
+public class Dealer extends Player {
+
+    // Instance Variable
+    // Define a deck of Cards
+
+    // Constructor
+    public Dealer() {
+
+    }
+
+    /**
+     * Shuffle the deck
+     */
+    public void shuffle() {
+        
+    }
+
+    /**
+     * Return the next card from the deck
+     */
+    public Card deal() {
+        return null;
+    }
+
+    /**
+     * How many cards are left in the deck?
+     */
+    public int numCardsLeftInDeck() {
+        return 0;
+    }
+
+    /**
+     * If the dealer's hand value is greater than 17,
+     * Add a card to the dealer's hand and return true;
+     * Otherwise, return false
+     */
+    public boolean hit() {
+        return false;
+    }
+}
---
+++ blackjack4/Grader.java
@@ -0,0 +1,10 @@
+/****************************************
+ * DO NOT DELETE OR CHANGE THIS FILE!!! *
+ ****************************************/
+import static java.lang.System.*;
+
+public class Grader {
+    public static void main(String[] args) {
+        
+    }
+}
