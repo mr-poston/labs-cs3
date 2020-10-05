b34a7e2e3f2569a1261d76179dcb6ed54fc0244d
mr-poston <john.poston@risd.org>
2020-10-05 16:33:55 +0000

    Fix Grader

--- vehicles/Grader.java
+++ vehicles/Grader.java
@@ -7,19 +7,19 @@ public class Grader {
     public static void main(String[] args) {
        if (args[0].equals("car")) {
            Vehicle car = new Car("Honda Civic", 2009, 9000.0, 32.0);
-           out.println(car);
+           out.println(car.getInfo());
        }
        if (args[0].equals("truck")) {
-           Vehicle truck = new Truck("Chevy Siverado", 2014, 18000.0, 5000);
-           out.println(truck);
+           Vehicle truck = new Truck("Chevy Silverado", 2014, 18000.0, 5000);
+           out.println(truck.getInfo());
        }
        if (args[0].equals("airplane")) {
            Vehicle plane = new Airplane("Cessna 206", 1997, 440000.0, 6, true);
-           out.println(plane);
+           out.println(plane.getInfo());
        }
        if (args[0].equals("helicopter")) {
            Vehicle heli = new Helicopter("Bell 206L", 1986, 660000.0, 7, false);
-           out.println(heli);
+           out.println(heli.getInfo());
        }
     }
 }
