1907cc31d1c8919a267d357c5c44277c54893a17
mr-poston <john.poston@risd.org>
2020-10-29 15:38:49 +0000

    Add Grader.java

---
+++ oddeven/Grader.java
@@ -0,0 +1,11 @@
+/****************************************
+ * DO NOT DELETE OR CHANGE THIS FILE!!! *
+ ****************************************/
+import static java.lang.System.*;
+
+public class Grader {
+    public static void main(String[] args) {
+        OddEvenSets test = new OddEvenSets(args[0]);
+        System.out.println(test);
+    }
+}
