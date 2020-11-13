c94169d68667ba7eec3378516712dc5cd0fdced4
mr-poston <john.poston@risd.org>
2020-11-13 15:48:09 +0000

    Add Spanish to English

--- spanish/SpanishToEnglish.java
+++ spanish/SpanishToEnglish.java
@@ -6,16 +6,22 @@ public class SpanishToEnglish {
     private Map<String, String> pairs;
 
     public SpanishToEnglish() {
+        // Instantiate `pairs` as a TreeMap
 
     }
 
     public void putEntry(String entry) {
+        // String `entry` will be two words, separated by a space
+        // The first word should be used as the key
+        // The second word should be used as the value
 
     }
 
-    public String translate(String sent) {
-        Scanner scan = new Scanner(sent);
+    public String translate(String sentence) {
+        Scanner scan = new Scanner(sentence);
         String output = "";
+        // Scan through `sentence` using each word as a key fo values in `pairs`
+        // Remember to add a space between each word in the output.
 
 
         return output;
