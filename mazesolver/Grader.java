91ba493c8b93eef355a0e65cf9eccbc3f7151d7b
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-04-16 11:04:45 -0500

    Add files via upload

--- mazesolver/Grader.java
+++ mazesolver/Grader.java
@@ -6,34 +6,42 @@ public class Grader {
     public static void main(String[] args) {
         if (args[0].equals("0")) {
             Maze test = new Maze(5, "1 0 0 0 1 1 1 1 1 0 0 0 1 0 1 0 1 1 1 0 0 0 0 0 1");
+            test.checkForExitPath(0, 0);
             System.out.println(test.result());
         }
         if (args[0].equals("1")) {
             Maze test = new Maze(7, "1 0 0 0 0 1 1 1 1 1 1 0 1 0 0 0 1 0 0 1 0 0 1 1 1 0 1 0 0 1 0 1 0 1 0 0 1 0 1 1 1 0 0 1 0 1 0 0 1");
+            test.checkForExitPath(0, 0);
             System.out.println(test.result());
         }
         if (args[0].equals("2")) {
             Maze test = new Maze(7, "1 0 0 0 0 1 0 1 1 1 1 0 1 0 0 0 1 0 0 1 0 0 1 1 1 0 1 0 0 1 0 1 0 1 0 0 1 0 1 1 1 0 0 1 0 1 0 1 0");
+            test.checkForExitPath(0, 0);
             System.out.println(test.result());
         }
         if (args[0].equals("3")) {
             Maze test = new Maze(7, "1 0 1 1 0 1 0 1 1 1 1 1 1 0 0 0 1 0 0 0 1 0 1 1 1 1 1 1 0 1 0 1 0 1 0 1 1 1 1 1 1 0 0 1 0 1 0 1 0");
+            test.checkForExitPath(0, 0);
             System.out.println(test.result());
         }
         if (args[0].equals("4")) {
             Maze test = new Maze(10, "1 0 1 1 0 1 0 1 1 1 1 1 1 1 1 1 0 1 0 1 0 0 1 0 0 0 1 1 1 1 0 1 1 1 1 1 1 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1 1 1 0 1 1 1 0 1 0 1 0 1 0 0 0 1 0 1 1 1 0 1 0 0 0 0 0 1 0 1 0 1 0 1 1 1 0 1 1 1 1 1 0 1 1 1");
+            test.checkForExitPath(0, 0);
             System.out.println(test.result());
         }
         if (args[0].equals("5")) {
             Maze test = new Maze(10, "1 0 1 1 0 1 1 1 1 0 1 1 1 1 1 1 0 1 0 1 0 0 1 0 0 0 1 1 1 0 0 1 1 1 1 1 1 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1 1 1 0 1 1 0 0 1 0 1 0 1 0 0 0 0 0 1 1 1 0 1 0 0 1 1 0 1 0 1 0 1 0 1 1 1 0 1 1 1 1 1 0 1 1 1");
+            test.checkForExitPath(0, 0);
             System.out.println(test.result());
         }
         if (args[0].equals("6")) {
             Maze test = new Maze(4, "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1");
+            test.checkForExitPath(0, 0);
             System.out.println(test.result());
         }
         if (args[0].equals("7")) {
             Maze test = new Maze(4, "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
+            test.checkForExitPath(0, 0);
             System.out.println(test.result());
         }
     }
