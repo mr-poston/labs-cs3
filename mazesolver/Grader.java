8cea6f286e350cc421d70cb55399627bf8bf5af9
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-04-16 10:51:24 -0500

    Add files via upload

---
+++ mazesolver/Grader.java
@@ -0,0 +1,40 @@
+/****************************************
+ * DO NOT DELETE OR CHANGE THIS FILE!!! *
+ ****************************************/
+import static java.lang.System.*;
+public class Grader {
+    public static void main(String[] args) {
+        if (args[0].equals("0")) {
+            Maze test = new Maze(5, "1 0 0 0 1 1 1 1 1 0 0 0 1 0 1 0 1 1 1 0 0 0 0 0 1");
+            System.out.println(test.result());
+        }
+        if (args[0].equals("1")) {
+            Maze test = new Maze(7, "1 0 0 0 0 1 1 1 1 1 1 0 1 0 0 0 1 0 0 1 0 0 1 1 1 0 1 0 0 1 0 1 0 1 0 0 1 0 1 1 1 0 0 1 0 1 0 0 1");
+            System.out.println(test.result());
+        }
+        if (args[0].equals("2")) {
+            Maze test = new Maze(7, "1 0 0 0 0 1 0 1 1 1 1 0 1 0 0 0 1 0 0 1 0 0 1 1 1 0 1 0 0 1 0 1 0 1 0 0 1 0 1 1 1 0 0 1 0 1 0 1 0");
+            System.out.println(test.result());
+        }
+        if (args[0].equals("3")) {
+            Maze test = new Maze(7, "1 0 1 1 0 1 0 1 1 1 1 1 1 0 0 0 1 0 0 0 1 0 1 1 1 1 1 1 0 1 0 1 0 1 0 1 1 1 1 1 1 0 0 1 0 1 0 1 0");
+            System.out.println(test.result());
+        }
+        if (args[0].equals("4")) {
+            Maze test = new Maze(10, "1 0 1 1 0 1 0 1 1 1 1 1 1 1 1 1 0 1 0 1 0 0 1 0 0 0 1 1 1 1 0 1 1 1 1 1 1 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1 1 1 0 1 1 1 0 1 0 1 0 1 0 0 0 1 0 1 1 1 0 1 0 0 0 0 0 1 0 1 0 1 0 1 1 1 0 1 1 1 1 1 0 1 1 1");
+            System.out.println(test.result());
+        }
+        if (args[0].equals("5")) {
+            Maze test = new Maze(10, "1 0 1 1 0 1 1 1 1 0 1 1 1 1 1 1 0 1 0 1 0 0 1 0 0 0 1 1 1 0 0 1 1 1 1 1 1 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1 1 1 0 1 1 0 0 1 0 1 0 1 0 0 0 0 0 1 1 1 0 1 0 0 1 1 0 1 0 1 0 1 0 1 1 1 0 1 1 1 1 1 0 1 1 1");
+            System.out.println(test.result());
+        }
+        if (args[0].equals("6")) {
+            Maze test = new Maze(4, "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1");
+            System.out.println(test.result());
+        }
+        if (args[0].equals("7")) {
+            Maze test = new Maze(4, "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
+            System.out.println(test.result());
+        }
+    }
+}
