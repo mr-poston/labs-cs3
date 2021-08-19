cf093b2056f7bd04b3d610f89bd6e09cce44a2d7
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-08-18 20:44:15 -0500

    Create IteratorRemover.java

---
+++ remove/IteratorRemover.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Read in a list of words and a word to remove from the list.
+ * Remove all occurrences of the word from list.
+ * You must use an Iterator
+ */
+
+public class IteratorRemover {
+    private ArrayList<String> list;
+    private String toRemove;
+
+    /**
+     * Constructor for IteratorRemover:
+     * Initialize instance variables here.
+     * list should be an empty ArrayList
+     * toRemove should be an empty String
+     */
+    public IteratorRemover() {
+
+    }
+
+    /**
+     * Use this method to set the list and the
+     * word to be removed.
+     */
+    public void setTest(String line, String rem) {
+
+    }
+
+    /**
+     * Removes all occurrences of toRemove from list
+     */
+    public void remove() {
+
+    }
+
+    /**
+     * Returns list as a String
+     */
+    @Override
+    public String toString() {
+        return """;
+    }
+}
