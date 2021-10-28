b37846c026614fc221997a543d55280805071a25
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-10-28 12:53:23 -0500

    Add files via upload

---
+++ pqtest/PQTester.java
@@ -0,0 +1,48 @@
+import java.util.Queue;
+import java.util.PriorityQueue;
+
+public class PQTester {
+	private Queue<String> pQueue;
+
+	/**
+	 * Call setPQ with an empty String
+	 */
+	public PQTester() {
+		
+	}
+
+	/**
+	 * Call setPQ with list
+	 */
+	public PQTester(String list) {
+		
+	}
+
+	/**
+	 * Initialize pQueue and
+	 * add all tokens from list (in order)
+	 */
+	public void setPQ(String list) {
+		
+	}
+	
+	/**
+	 * Return the smallest value in pQueue
+	 */
+	public String getMin() {
+		
+	}
+	
+	/**
+	 * Return a String of all values in pQueue
+	 * in natural order with a space after each element
+	 */
+	public String getNaturalOrder() {
+				
+	}
+
+	/**
+	 * Write a toString method below!
+	 */
+	
+}
---
+++ pqtest/PQTesterRunner.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PQTesterRunner
+{
+	public static void main ( String[] args )
+	{
+	    // one two three four five six seven
+	    // 1 2 3 4 5 one two three four five
+	    // a p h j e f m c i d k l g n o b
+
+		PQTester test = new PQTester();
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Enter a list of Strings: ");
+		String list = kb.nextLine();
+		test.setPQ(list);
+		System.out.println(test);
+		System.out.println(test.getMin());
+		System.out.println(test.getNaturalOrder());
+	}
+}
